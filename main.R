e75958b7af11a0e9b9297428668f6e824a914db6
Shark-dev-star <mjohnfissher@gmail.com>
2026-02-03 14:19:28 +0300

    filtered for those less than 30.R

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ pacman::p_load(tidyverse,
                here)
 
 tv_hours_analysis <- gss_cat %>%
-  filter(age >= 30) %>%
+  filter(age < 30) %>%
   group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = T))
 tv_hours_analysis
