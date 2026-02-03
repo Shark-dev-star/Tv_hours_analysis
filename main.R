20261603480bd1af5ce72dc40940e4584b583e87
Shark-dev-star <mjohnfissher@gmail.com>
2026-02-03 14:47:52 +0300

    changes made

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ tv_hours_tabulation <- gss_cat %>%
   filter(age < 30) %>%
   group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = T))
-tv_hours_analysis
+tv_hours_tabulation
 
 write.csv(tv_hours_tabulation, here("tv_hours_spent.csv"))
 
