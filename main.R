390026e1f096a72f51a6d029cfe54190e52da6da
Shark-dev-star <mjohnfissher@gmail.com>
2026-02-03 13:24:01 +0300

    Started analysis

---
+++ main.R
@@ -0,0 +1,10 @@
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(tidyverse,
+               here)
+
+tv_hours_analysis <- gss_cat %>% 
+  group_by(marital) %>% 
+  summarize(mean_tv_hours = mean(tvhours, na.rm = T))
+tv_hours_analysis
+
+write.csv(tv_hours_analysis, here("tv_hours_spent.csv"))
