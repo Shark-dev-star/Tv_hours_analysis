f373fce3c1141375acdf4e9b9c47c517d5c145bf
Shark-dev-star <mjohnfissher@gmail.com>
2026-02-03 13:38:40 +0300

    filtered to those 30 and above

--- main.R
+++ main.R
@@ -2,9 +2,12 @@ if(!require(pacman)) install.packages("pacman")
 pacman::p_load(tidyverse,
                here)
 
-tv_hours_analysis <- gss_cat %>% 
+tv_hours_analysis <- gss_cat %>%
+  filter(age >= 30) %>%
   group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = T))
 tv_hours_analysis
 
 write.csv(tv_hours_analysis, here("tv_hours_spent.csv"))
+
+
