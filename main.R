d01d6e9f6f84a323a1442bd26b675f83f960328e
Shark-dev-star <mjohnfissher@gmail.com>
2026-02-03 14:37:03 +0300

    shorter naming

--- main.R
+++ main.R
@@ -2,12 +2,12 @@ if(!require(pacman)) install.packages("pacman")
 pacman::p_load(tidyverse,
                here)
 
-tv_hours_analysis <- gss_cat %>%
+tv_hours_tab <- gss_cat %>%
   filter(age < 30) %>%
   group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = T))
 tv_hours_analysis
 
-write.csv(tv_hours_analysis, here("tv_hours_spent.csv"))
+write.csv(tv_hours_tab, here("tv_hours_spent.csv"))
 
 
